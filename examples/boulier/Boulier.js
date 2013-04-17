766281ca9235a6d229ddd81b6c1410a0da416e0e
David Thevenin <david.thevenin@gmail.com>
2013-04-17 16:19:05 +0900

    Update demo

--- examples/boulier/Boulier.js
+++ examples/boulier/Boulier.js
@@ -3,43 +3,43 @@ var Animations = vs.core.createClass ({
   /** parent class */
   parent: vs.ui.Application,
 
-  initComponent : function () {
-    this._super ();
-
-    this.ball1 = new vs.ui.View ({id:'ball1'}).init ();
-    this.ball2 = new vs.ui.View ({id:'ball2'}).init ();
-    this.ball3 = new vs.ui.View ({id:'ball3'}).init ();
-    this.ball4 = new vs.ui.View ({id:'ball4'}).init ();
-  },
-
   applicationStarted : function (event) {
   
+    this.ball1 = new vs.ui.View ({id:'ball1'}).init ();
+    this.ball2 = new vs.ui.View ({id:'ball4'}).init ();
+    
+    function getPace () { return new Pace ({
+        timing: cubicBezierTransition (0.035, 0.715, 0.96, 0.235)
+      }).init ()
+    };
+
     this.anim1 = animateTransition (this.ball1, 'translation', {
-      duration: 300,
-      pace: Pace.getEaseInPace (),
+      duration: 400,
+      pace: getPace (),
       trajectory: new Circular2D ({
         center: [0,-200],
-        values: [45, 60, 45]
+        values: [45, 55, 45]
       }).init ()
     });
     
-    this.anim2 = animateTransition (this.ball4, 'translation', {
-      duration: 300,
-      pace: Pace.getEaseInPace (),
+    this.anim2 = animateTransition (this.ball2, 'translation', {
+      duration: 400,
+      pace: getPace (),
       trajectory: new Circular2D ({
         center: [0,-200],
-        values: [45, 30, 45]
+        values: [45, 35, 45]
       }).init ()
     });
     
-   var seq = vs.seq (
-    this.anim1, this.anim2, this.anim1, this.anim2, this.anim1, this.anim2, this.anim1);
+   var seq = vs.seq (this.anim1, this.anim2);
+   seq.delegate = {
+    taskDidEnd: function () {seq.start ()}
+   }
    seq.start ();
   },
 });
 
 function loadApplication () {
   new Animations ({id:"animations", layout:vs.ui.View.ABSOLUTE_LAYOUT}).init ();
-
   vs.ui.Application.start ();
 }
