810528b38a6a28ab3b468a236d058420f8a26c1d
David Thevenin <david.thevenin@gmail.com>
2013-04-24 17:36:52 +0900

    Add new animation helper

--- src/AnimatedTransition.js
+++ src/AnimatedTransition.js
@@ -68,3 +68,32 @@ exports.animateTransition = function (obj, property, options)
   
   return chrono;
 }
+
+exports.animateTransitionBis = function (obj, srcs, targets, options)
+{
+  if (!vs.util.isArray (srcs) || !vs.util.isArray (targets)) return;
+  if (srcs.length !== targets.length) return;
+  
+  var animOptions = vs.util.clone (AnimationDefaultOption);
+  if (options)
+  {
+    for (var key in options) animOptions [key] = options [key];
+  }
+  
+  var chrono = new Chronometer (animOptions).init ();
+  var pace = animOptions.pace;
+  var traj = animOptions.trajectory;
+
+  chrono.__clb = function (i) {
+    pace.tickIn = i;
+    pace.compute ();
+    
+    traj.tick = pace.tickOut;
+    traj.compute ();
+    
+    for (var i = 0; i < srcs.length; i++)
+    obj [targets[i]] = traj [srcs[i]];
+  }
+  
+  return chrono;
+}
